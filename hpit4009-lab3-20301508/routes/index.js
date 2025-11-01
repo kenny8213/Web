870431d43a919a5f6e7c91c6cd81b086acccb7f1
kenny8213 <kenchan8213@gmail.com>
2025-11-01 10:40:49 +0800

    function to =>

--- hpit4009-lab3-20301508/routes/index.js
+++ hpit4009-lab3-20301508/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express (Chan Ka Ho ......)' }); 
+  res.render('index', { title: 'Express (Chan Ka Ho .....)' }); 
 });
 
 module.exports = router;
